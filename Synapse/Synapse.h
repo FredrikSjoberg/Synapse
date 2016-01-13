869bda7b8e844f8e6f779a4a764a122d969d7b8e
Fredrik Sjöberg <fredrik.sjoberg@knightsfee.com>
2016-01-13 12:16:27 +0100

    Initial Commit

---
+++ Synapse/Synapse.h
@@ -0,0 +1,19 @@
+//
+//  Synapse.h
+//  Synapse
+//
+//  Created by Fredrik Sjöberg on 2016-01-13.
+//  Copyright © 2016 Fredrik Sjöberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Synapse.
+FOUNDATION_EXPORT double SynapseVersionNumber;
+
+//! Project version string for Synapse.
+FOUNDATION_EXPORT const unsigned char SynapseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Synapse/PublicHeader.h>
+
+
